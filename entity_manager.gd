b7b586abd7f11e1b4cfcc86b587b51438c7c9656
snipercup <50166150+snipercup@users.noreply.github.com>
2025-07-15 21:06:33 +0200

    Fix indentation and revert test env changes

--- entity_manager.gd
+++ entity_manager.gd
@@ -3,7 +3,8 @@ extends Node3D
 @onready var projectiles_container : Node = $Projectiles
 
 func _init():
-	Helper.signal_broker.projectile_spawned.connect(on_projectile_spawned)
+	Helper.signal_broker.projectile_spawned.connect(_on_projectile_spawned)
 
-func on_projectile_spawned(projectile: Node, instigator: RID):
+# Add spawned projectiles to the dedicated container
+func _on_projectile_spawned(projectile: Node, _instigator: RID) -> void:
 	projectiles_container.add_child(projectile)
