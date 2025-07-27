d494760053c0e72daff7d13c06c0f3ad5fd6790f
snipercup <50166150+snipercup@users.noreply.github.com>
2025-07-27 20:22:38 +0200

    remove test

--- Tests/Unit/test_overmap_area_generator.gd
+++
@@ -1,7 +0,0 @@
-extends GutTest
-
-
-func test_pick_compatible_candidate_returns_null_on_empty():
-	var gen = OvermapAreaGenerator.new()
-	var result = gen._pick_compatible_candidate(null, "north", Vector2.ZERO, [])
-	assert_eq(result, null)
