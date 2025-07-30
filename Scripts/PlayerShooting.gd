980f5b22235e350c48a5fbca9e7cb7abcf4a94b6
snipercup <50166150+snipercup@users.noreply.github.com>
2025-07-30 18:53:47 +0200

    Use signal for reloading

--- Scripts/PlayerShooting.gd
+++ Scripts/PlayerShooting.gd
@@ -12,6 +12,7 @@ var _bullet_count: int = 0
 
 func _ready() -> void:
 	Helper.signal_broker.projectile_spawned.connect(_on_projectile_spawned)
+	PlayerInputSignalBroker.reload_weapon().connect(_on_reload_weapon)
 
 
 func _on_projectile_spawned(projectile: Node, instigator: RID) -> void:
@@ -27,34 +28,23 @@ func reset_bullet_count() -> void:
 	_bullet_count = 0
 
 
-func _input(event):
-	# Return early if no weapons are equipped
+func _on_reload_weapon() -> void:
 	if not left_hand_item or not right_hand_item:
 		return
-
-	if event.is_action_pressed("reload_weapon"):
-		if General.is_action_in_progress:
-			return
-		# Check if both weapons can be reloaded
-		if left_hand_item.can_weapon_reload() and right_hand_item.can_weapon_reload():
-			# Compare the current ammo to decide which weapon to reload
-			if left_hand_item.get_current_ammo() < right_hand_item.get_current_ammo():
-				left_hand_item.reload_weapon()
-			elif left_hand_item.get_current_ammo() > right_hand_item.get_current_ammo():
-				right_hand_item.reload_weapon()
-			else:
-				# If both have equal ammo, reload the left hand first
-				if left_hand_item.can_weapon_reload():
-					# Only the left hand weapon can be reloaded
-					left_hand_item.reload_weapon()
-				elif right_hand_item.can_weapon_reload():
-					# Only the right hand weapon can be reloaded
-					right_hand_item.reload_weapon()
+	if General.is_action_in_progress:
+		return
+	if left_hand_item.can_weapon_reload() and right_hand_item.can_weapon_reload():
+		if left_hand_item.get_current_ammo() < right_hand_item.get_current_ammo():
+			left_hand_item.reload_weapon()
+		elif left_hand_item.get_current_ammo() > right_hand_item.get_current_ammo():
+			right_hand_item.reload_weapon()
 		else:
-			# If both have equal ammo, reload the left hand first
 			if left_hand_item.can_weapon_reload():
-				# Only the left hand weapon can be reloaded
 				left_hand_item.reload_weapon()
 			elif right_hand_item.can_weapon_reload():
-				# Only the right hand weapon can be reloaded
 				right_hand_item.reload_weapon()
+	else:
+		if left_hand_item.can_weapon_reload():
+			left_hand_item.reload_weapon()
+		elif right_hand_item.can_weapon_reload():
+			right_hand_item.reload_weapon()
